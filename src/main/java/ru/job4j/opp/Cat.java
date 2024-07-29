d62a4ff8c0736814267f9e2a230b72a3554e7212
JeDiFoXxX <serjik1993991@gmail.com>
2024-07-29 09:19:51 +0300

    Вызов метода с возвращаемым типом

--- src/main/java/ru/job4j/opp/Cat.java
+++ src/main/java/ru/job4j/opp/Cat.java
@@ -1,7 +1,13 @@
 package ru.job4j.opp;
 
 public class Cat {
+    public String sound() {
+        String voice = "may-may";
+        return voice;
+    }
+
     public static void main(String[] args) {
         Cat peppy = new Cat();
+        System.out.println(peppy.sound());
     }
 }
