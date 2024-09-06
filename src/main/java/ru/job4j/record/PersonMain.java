3778efdc2136c0850697366ab78bf4b773914f84
JeDiFoXxX <serjik1993991@gmail.com>
2024-09-06 16:55:28 +0300

    MySetting Person-Record

---
+++ src/main/java/ru/job4j/record/PersonMain.java
@@ -0,0 +1,16 @@
+package ru.job4j.record;
+
+public class PersonMain {
+    public static void main(String[] args) {
+        Person person = new Person("Иван", 30);
+        System.out.println("Имя: " + person.getName() + ", Возраст: " + person.getAge());
+        System.out.println(person);
+        PersonRecord record = new PersonRecord("Иван", 30);
+        System.out.println("Имя: " + record.name() + ", Возраст: " + record.age());
+        System.out.println(record);
+        PersonRecord record1 = new PersonRecord("Иван", 30);
+        System.out.println(record.equals(record1));
+        PersonRecord newRecord1 = new PersonRecord("Сергей", record.age());
+        System.out.println(newRecord1.name() + "........"  + newRecord1.age());
+    }
+}
