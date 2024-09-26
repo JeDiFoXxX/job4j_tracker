40d0527d4ee498a56005b5196a99a029cf780791
JeDiFoXxX <serjik1993991@gmail.com>
2024-09-26 07:47:34 +0300

    Именование классов реализующих интерфейс importRefactor

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,10 +2,8 @@ package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
 import ru.job4j.tracker.action.*;
-import ru.job4j.tracker.input.Input;
-import ru.job4j.tracker.input.MockInput;
-import ru.job4j.tracker.output.Output;
-import ru.job4j.tracker.output.StubOutput;
+import ru.job4j.tracker.input.*;
+import ru.job4j.tracker.output.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
