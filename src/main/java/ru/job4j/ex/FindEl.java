8f91b5022abe47ff8b5bfdd901e351f9c158cc31
JeDiFoXxX <serjik1993991@gmail.com>
2024-09-26 20:15:44 +0300

    Пользовательские исключения

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,24 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (value[index].equals(key)) {
+                result = index;
+                break;
+            } else {
+                throw new ElementNotFoundException("The element was not found");
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(indexOf(new String[]{"1", "2"}, "0"));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
