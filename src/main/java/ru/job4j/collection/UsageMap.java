3db5cc90a002f8b5ef448ff353478b4296d48f3c
JeDiFoXxX <serjik1993991@gmail.com>
2024-11-09 18:47:42 +0300

    Map, HashMap

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,9 @@ public class UsageMap {
     public static void main(String[] args) {
         Map<String, String> map = new HashMap<>();
         map.put("Chaplin@gmail.com", "Charlie Chaplin");
+        map.put("Silver@gmail.com", "Johnny Silverhand");
+        map.put("David@gmail.com", "David Duchovny");
+        map.put("David@gmail.com", "David Martinez");
 
         for (String key : map.keySet()) {
             System.out.println(key + ": " + map.get(key));
