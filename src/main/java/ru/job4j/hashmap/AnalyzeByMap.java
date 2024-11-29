3cf0ace1edf7a6a1984f0669fda3daaf44d5abd6
JeDiFoXxX <serjik1993991@gmail.com>
2024-11-29 19:54:16 +0300

    Лямбда выражения и Collection Framework

--- src/main/java/ru/job4j/hashmap/AnalyzeByMap.java
+++ src/main/java/ru/job4j/hashmap/AnalyzeByMap.java
@@ -34,7 +34,7 @@ public class AnalyzeByMap {
         Map<String, Double> map = new LinkedHashMap<>();
         for (Pupil pupil : pupils) {
             for (Subject subject : pupil.subjects()) {
-                map.put(subject.name(), map.getOrDefault(subject.name(), 0D) + subject.score());
+                map.merge(subject.name(), (double) subject.score(), Double::sum);
             }
         }
 
@@ -62,7 +62,7 @@ public class AnalyzeByMap {
         Map<String, Double> map = new LinkedHashMap<>();
         for (Pupil pupil : pupils) {
             for (Subject subject : pupil.subjects()) {
-                map.put(subject.name(), map.getOrDefault(subject.name(), 0D) + subject.score());
+                map.merge(subject.name(), (double) subject.score(), Double::sum);
             }
         }
 
